76b91915aa6c01bf8b1161aea80206af6d468296
zaidanshori <m.zaidanshori04@gmail.com>
2026-04-23 14:29:07 +0700

    feat: inisiasi awal halaman beranda utama

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.render("index", {title: "beranda"})
 })
 
 const PORT = process.env.PORT || 3000;
