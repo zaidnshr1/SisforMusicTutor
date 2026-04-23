ca02a31abb7b47c94da840bacb5b01856c5715e5
zaidanshori <m.zaidanshori04@gmail.com>
2026-04-23 11:12:38 +0700

    chore: initial project and server configure

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__fileName);
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirName, "views"));
+
+app.use(express.static(path.join(__dirName, "../public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+})
