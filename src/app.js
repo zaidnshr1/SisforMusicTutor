ef1938f3223784264fabc6fc2eeae9eb79939c16
zaidanshori <m.zaidanshori04@gmail.com>
2026-04-23 15:26:03 +0700

    feat: menambah session cookie durasi 1 hari

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
+import session from "express-session";
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = path.dirname(__fileName);
@@ -14,6 +15,18 @@ app.set("views", path.join(__dirName, "views"));
 app.use(express.static(path.join(__dirName, "../public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 1000 * 60 * 60 * 24, // 24 jam or 1 hari
+        },
+    })
+)
 
 app.get("/", (req, res) => {
     res.render("index", {title: "beranda"})
